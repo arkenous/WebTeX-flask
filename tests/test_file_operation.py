30ded0396fc29436936eccb91d70d530a4e9e741
Kensuke Kosaka <k@trileg.net>
2016-02-01 22:43:50 +0900

    Update test_file_operation.py

--- tests/test_file_operation.py
+++ tests/test_file_operation.py
@@ -3,12 +3,16 @@
 
 from nose.tools import eq_, ok_
 import WebTeX
+import os
 import json
 from io import BytesIO
+from configparser import ConfigParser
 
 WebTeX.app.testing = True
 client = WebTeX.app.test_client()
-
+conf_path = os.path.dirname(os.path.abspath(__file__)) + '/../WebTeX.ini'
+redpen_conf_path = os.path.expanduser('~/redpen/conf/redpen-conf-en.xml')
+java_home = '/usr/lib/jvm/java-8-oracle'
 
 def test_login():
     res = client.post('/login', data={
@@ -91,3 +95,24 @@ def test_read_filelist_uploaded_tex():
     eq_('Success', data['result'])
     ok_(len(data['list']) != 0)
     eq_('True', data['tex'])
+
+
+def test_compile_tex():
+    config = ConfigParser()
+    config.read(conf_path)
+    config['redpen']['conf'] = redpen_conf_path
+    config['redpen']['java_home'] = java_home
+    with open(conf_path, 'w') as configfile:
+        config.write(configfile)
+    sample_tex_text = (
+        r'\documentclass{jarticle}''\n'
+        r'\begin{document}''\n'
+        r'This is sample tex file.''\n'
+        r'\end{document}''\n'
+    )
+    res = client.post('/compile',
+                      data=json.dumps(dict(text=sample_tex_text)),
+                      content_type='application/json')
+    data = json.loads(json.loads(res.data.decode('utf-8'))['ResultSet'])
+    eq_(200, res.status_code)
+    eq_('Success', data['result'])
