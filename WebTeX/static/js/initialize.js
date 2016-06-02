e60e81175cb73397791a5494c9e4a7342cdd1b2b
Kensuke Kosaka <k@trileg.net>
2016-06-02 12:36:06 +0900

    Add initialize.html, js, css

---
+++ WebTeX/static/js/initialize.js
@@ -0,0 +1,68 @@
+$(window).load(init());
+
+function init() {
+  $(".ldap").hide();
+  readConfig();
+
+  $("input[name='mode']:radio").change(function() {
+    if ($(this).val() == 1) {
+      $(".ldap").hide("normal");
+    } else {
+      $(".ldap").show("normal");
+    }
+  });
+  $("#OK").click(function (event) {
+    event.preventDefault();
+    saveConfig();
+  });
+}
+
+
+function readConfig() {
+  $.ajax({
+    type: 'POST',
+    url: '/readConfig',
+    contentType: 'application/json',
+    success: function(data) {
+      var parsed = JSON.parse(data.ResultSet);
+      var result = parsed.result;
+      if (result != "Failure") {
+        $("#ldap_address").val(parsed.ldap_address);
+        $("#ldap_port").val(parsed.ldap_port);
+        $("#ldap_basedn").val(parsed.ldap_basedn);
+        $("#java_home").val(parsed.java_home);
+        $("#redpen_path").val(parsed.redpen_conf_path);
+      }
+    }
+  });
+  return false;
+}
+
+
+function saveConfig() {
+  var ldap_address = $("#ldap_address").val();
+  var ldap_port = $("#ldap_port").val();
+  var ldap_basedn = $("#ldap_basedn").val();
+
+  var java_home = $("#java_home").val();
+  var redpen_conf_path = $("#redpen_path").val();
+
+  var json = JSON.stringify({
+    "ldap_address": ldap_address,
+    "ldap_port": ldap_port,
+    "ldap_basedn": ldap_basedn,
+    "java_home": java_home,
+    "redpen_conf_path": redpen_conf_path
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: '/saveConfig',
+    data: json,
+    contentType: 'application/json',
+    success: function() {
+      console.log("save config succeed");
+    }
+  });
+  return false;
+}
