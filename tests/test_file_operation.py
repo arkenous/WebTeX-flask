b56ed3bb3616fbd26c21ea46d8379673373ad806
Kensuke Kosaka <k@trileg.net>
2016-06-04 17:15:17 +0900

    Update test_file_operation

--- tests/test_file_operation.py
+++ tests/test_file_operation.py
@@ -7,23 +7,35 @@ from WebTeX import app
 import json
 from io import BytesIO
 from configparser import ConfigParser
+import sqlite3
+from werkzeug.security import generate_password_hash as gen_pass_hash
 
 app.testing = True
 client = app.app.test_client()
 webtex_path = os.path.dirname(os.path.abspath(__file__)) + '/../WebTeX/'
 conf_path = webtex_path + 'WebTeX.ini'
+db_path = webtex_path + 'WebTeX.db'
 redpen_conf_path = os.path.expanduser('~/redpen/conf/redpen-conf-en.xml')
 java_home = '/usr/lib/jvm/java-8-oracle'
 
 
 def setup():
-    conf_dict = {'mode': 'local', 'ldap_address': '', 'ldap_port': '',
+    res = client.post('/login', data={
+        'username': 'Admin',
+        'password': 'webtex'
+    })
+    eq_(302, res.status_code)
+    eq_('http://localhost/initialize', res.headers['Location'])
+
+    conf_dict = {'user_name': 'test-user', 'user_password': 'test-pass',
+                 'mode': 'local', 'ldap_address': '', 'ldap_port': '',
                  'ldap_basedn': '', 'java_home': '/usr/lib/jvm/java-8-oracle',
                  'redpen_conf_path': os.path.expanduser(
                      '~/redpen/conf/redpen-conf-en.xml')}
-    client.post('/saveConfig',
-                data=json.dumps(conf_dict),
-                content_type='application/json')
+    res = client.post('/saveConfig',
+                      data=json.dumps(conf_dict),
+                      content_type='application/json')
+    eq_(200, res.status_code)
 
 
 def teardown():
@@ -34,15 +46,23 @@ def teardown():
     config.write(f)
     f.close()
 
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    sql = 'UPDATE user SET username=(?), password=(?) WHERE username=(?)'
+    cur.execute(sql, ('Admin', gen_pass_hash('webtex'), 'test-user',))
+    con.commit()
+    cur.close()
+    con.close()
+
     res = client.get('/')
     eq_(302, res.status_code)
-    eq_('http://localhost/initialize', res.headers['Location'])
-
-    res = client.get('/login')
-    eq_(302, res.status_code)
-    eq_('http://localhost/initialize', res.headers['Location'])
+    eq_('http://localhost/login', res.headers['Location'])
 
     res = client.get('/initialize')
+    eq_(302, res.status_code)
+    eq_('http://localhost/login', res.headers['Location'])
+
+    res = client.get('/login')
     eq_(200, res.status_code)
 
 
@@ -55,8 +75,8 @@ def test_login():
     f.close()
 
     res = client.post('/login', data={
-        'username': 'Admin',
-        'password': 'webtex'
+        'username': 'test-user',
+        'password': 'test-pass'
     })
     eq_(302, res.status_code)
     eq_('http://localhost/', res.headers['Location'])
@@ -161,7 +181,7 @@ def test_compile_tex():
 
 def test_logout():
     with client.session_transaction() as sess:
-        eq_(sess['username'], 'Admin')
+        eq_(sess['username'], 'test-user')
     res = client.get('/logout')
     with client.session_transaction() as sess:
         eq_(sess, {})
