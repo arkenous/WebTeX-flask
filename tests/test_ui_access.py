3f9a91fdcefa8997fbcf1df97c3ebb3617faeabc
Kensuke Kosaka <k@trileg.net>
2016-06-04 17:58:58 +0900

    Update test_ui_access

--- tests/test_ui_access.py
+++ tests/test_ui_access.py
@@ -8,9 +8,12 @@ from selenium.webdriver.support import expected_conditions as ec
 from nose.tools import eq_
 import os
 from configparser import ConfigParser
+import sqlite3
+from werkzeug.security import generate_password_hash as gen_pass_hash
 
 webtex_path = os.path.dirname(os.path.abspath(__file__)) + '/../WebTeX/'
 conf_path = webtex_path + 'WebTeX.ini'
+db_path = webtex_path + 'WebTeX.db'
 
 
 def test_login_logout():
@@ -19,7 +22,34 @@ def test_login_logout():
 
     driver.get('http://localhost:8080/')
     wait.until(ec.presence_of_all_elements_located)
+    eq_('http://localhost:8080/login', driver.current_url)
+
+    driver.get('http://localhost:8080/initialize')
+    wait.until(ec.presence_of_all_elements_located)
+    eq_('http://localhost:8080/login', driver.current_url)
+
+    show_signin = driver.find_element_by_id('showSignIn')
+    show_signin.click()
+
+    wait.until(ec.visibility_of_element_located((By.ID, 'username')))
+    username = driver.find_element_by_id('username')
+    username.send_keys('Admin')
+
+    wait.until(ec.visibility_of_element_located((By.ID, 'password')))
+    password = driver.find_element_by_id('password')
+    password.send_keys('webtex')
+
+    wait.until(ec.visibility_of_element_located((By.ID, 'signIn')))
+    signin = driver.find_element_by_id('signIn')
+    signin.click()
+
+    wait.until(ec.presence_of_all_elements_located)
     eq_('http://localhost:8080/initialize', driver.current_url)
+
+    user_name = driver.find_element_by_id('user-name')
+    user_name.send_keys('test-user')
+    user_pass = driver.find_element_by_id('user-password')
+    user_pass.send_keys('test-pass')
 
     java_home = driver.find_element_by_id('java_home')
     java_home.send_keys('/usr/lib/jvm/java-8-oracle')
@@ -38,11 +68,11 @@ def test_login_logout():
 
     wait.until(ec.visibility_of_element_located((By.ID, 'username')))
     username = driver.find_element_by_id('username')
-    username.send_keys('Admin')
+    username.send_keys('test-user')
 
     wait.until(ec.visibility_of_element_located((By.ID, 'password')))
     password = driver.find_element_by_id('password')
-    password.send_keys('webtex')
+    password.send_keys('test-pass')
 
     wait.until(ec.visibility_of_element_located((By.ID, 'signIn')))
     signin = driver.find_element_by_id('signIn')
@@ -65,3 +95,12 @@ def test_login_logout():
     f = open(conf_path, 'w')
     config.write(f)
     f.close()
+
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    sql = 'UPDATE user SET username=(?), password=(?) WHERE username=(?)'
+    cur.execute(sql, ('Admin', gen_pass_hash('webtex'), 'test-user',))
+    con.commit()
+    cur.close()
+    con.close()
+
