b37dcb3c09d871a8ceeb5dda1a55bdd7e5822adc
Kensuke Kosaka <k@trileg.net>
2016-06-12 15:09:22 +0900

    Simplify conditional branch of before_request

--- WebTeX/app.py
+++ WebTeX/app.py
@@ -25,26 +25,18 @@ storage = base + '/static/storage/'
 
 @app.before_request
 def before_request():
-    if 'username' not in session:
-        if request.path == '/login':
-            return
+    if 'username' not in session and request.path != '/login':
         return redirect('/login')
 
     config = configparser.ConfigParser()
     config.read(conf)
     initial_setup = config['setup']['initial_setup']
-    if initial_setup == 'true' and request.path == '/initialize':
-        return
-    if initial_setup == 'true' and (
-            request.path == '/readConfig' or request.path == '/saveConfig'):
-        return
-    if initial_setup == 'true':
+    initial_setup_path = ['/initialize', '/readConfig', '/saveConfig']
+    if initial_setup == 'true' and request.path not in initial_setup_path:
         return redirect('/initialize')
-    if initial_setup == 'false' and request.path == '/initialize':
+    if initial_setup == 'false' and request.path in initial_setup_path:
         return redirect('/logout')
-    if initial_setup == 'false' and (
-            request.path == '/readConfig' or request.path == '/saveConfig'):
-        return redirect('/logout')
+
     return
 
 
