6953d353a3f12ce7953b658b97136c387c0c7cc4
Kensuke Kosaka <k@trileg.net>
2016-06-13 12:01:30 +0900

    Add register to preference.js

--- WebTeX/static/js/preference.js
+++ WebTeX/static/js/preference.js
@@ -13,7 +13,10 @@ function init() {
 
   readConfig();
 
-
+  $("#register").click(function (event) {
+    event.preventDefault();
+    register();
+  });
 }
 
 function readConfig() {
@@ -37,6 +40,44 @@ function readConfig() {
 
         $("#redpen-path").val(parsed.redpen_conf_path);
         $("#java-home").val(parsed.java_home);
+      }
+    }
+  });
+  return false;
+}
+
+function register() {
+  if (!$("#username").val() || !$("#password").val()) {
+    $("#result-user-registration").empty();
+    $("#result-user-registration").append(
+        '<p class="result bg-danger">Please input username and/or password</p>'
+    );
+    return false;
+  }
+
+  var json = JSON.stringify({
+    'username': $("#username").val(),
+    'password': $("#password").val()
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: '/registerUser',
+    data: json,
+    contentType: 'application/json',
+    success: function() {
+      var parsed = JSON.parse(data.ResultSet);
+      var result = parsed.result;
+      if (result === 'Failure') {
+        $("#result-user-registration").empty();
+        $("#result-user-registration").append(
+            '<p class="result bg-danger">'+parsed.cause+'</p>'
+        );
+      } else {
+        $("#result-user-registration").empty();
+        $("#result-user-registration").append(
+            '<p class="result bg-info">Successfully Registration</p>'
+        );
       }
     }
   });
